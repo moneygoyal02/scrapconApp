08f2ab55cf441905fe879ddc5ffbe6bd5cc72079
gurshaan17 <gurshaansinghd@gmail.com>
2025-02-08 22:48:46 +0530

    token coming in vendor

--- vendor_app/lib/main.dart
+++ vendor_app/lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'token_provider.dart'; 
 import 'screens/splash_screen.dart';
 
 void main() {
-  runApp(VendorApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => TokenProvider(),
+      child: VendorApp(),
+    ),
+  );
 }
 
 class VendorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Scrapcon',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: SplashScreen(),
+      title: 'Vendor App',
+      home: SplashScreen(), 
     );
   }
 }

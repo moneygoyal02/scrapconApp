f46356cb26b914cd0d1a6e42575dc05a15c2e48c
gurshaan17 <gurshaansinghd@gmail.com>
2025-02-09 03:17:56 +0530

    user profile page made

--- scrapcon/lib/screens/user_screens/user_message.dart
+++ scrapcon/lib/screens/user_screens/user_message.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;

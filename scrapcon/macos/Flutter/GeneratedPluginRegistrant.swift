ad1764c409c1d6e7d467300132cbd26c15927549
JAYANTJOSHI001 <jayantjoshi909@gmail.com>
2025-02-08 23:22:38 +0530

    updated code
    Signed-off-by: JAYANTJOSHI001 <jayantjoshi909@gmail.com>

--- scrapcon/macos/Flutter/GeneratedPluginRegistrant.swift
+++ scrapcon/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,14 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<<<< Temporary merge branch 1
+import file_picker
 import file_selector_macos
+import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-=========
-import url_launcher_macos
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
->>>>>>>>> Temporary merge branch 2
 }

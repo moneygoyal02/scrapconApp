977338ca22faf9cf8fd595bfa7a7a1853f583838
88VwV88 <dhimankkvivek773@gmail.com>
2025-02-08 14:39:58 +0530

    Add Vendor Screens Routes and Routing for the Vendor userflow

---
+++ scrapcon/lib/screens/vendor_screens/vendor_bottom_nav_bar.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+// import '../../vendor_screens/vendor_explore.dart';
+// import '../../vendor_screens/vendor_activity.dart';
+// import '../../vendor_screens/vendor_profile.dart';
+
+class VendorBottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  VendorBottomNavBar({required this.currentIndex, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      selectedIndex: currentIndex,
+      onDestinationSelected: onTap,
+      destinations: const <NavigationDestination>[
+        NavigationDestination(
+          selectedIcon: Icon(Icons.dashboard),
+          icon: Icon(Icons.dashboard_outlined),
+          label: 'Dashboard', 
+        ),
+        NavigationDestination(
+          selectedIcon: Icon(Icons.receipt),
+          icon: Icon(Icons.receipt_outlined),
+          label: 'Activity',
+        ),
+        NavigationDestination(
+          selectedIcon: Icon(Icons.explore),
+          icon: Icon(Icons.explore_outlined),
+          label: 'Explore',
+        ),
+        NavigationDestination(
+          selectedIcon: Icon(Icons.person),
+          icon: Icon(Icons.person_outlined),
+          label: 'Account',
+        ),
+      ],
+    );
+  }
+} 
---
+++ scrapcon/lib/screens/vendor_screens/vendor_explore.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class VendorExploreScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Explore Screen',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+} 
